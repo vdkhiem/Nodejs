0ff7ad5b9228d89101b1ac2dd16eddda51ffa982
Khiem <vdkhiem81@yahoo.com>
2016-08-24 12:50:25 +1200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		// new Date().toString();
+		console.log(req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+		next();
+	}
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+/*
+app.get('/', function(req, res){
+	//req may be cookie, url
+	res.send('Hello Express Web Server');
+});
+*/
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send("About Us")
+});
+
+// Get nodejs go inside public folder
+app.use(express.static(__dirname + '/public'));
+//console.log(__dirname); //Get server foder
+
+// Tell browser to run on port 3000
+app.listen(port, function(){
+	console.log('Express server started at port:' + port);
+});
